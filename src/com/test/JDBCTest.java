b6f67f60e43529a101928a43e924cc301d4431e7
jmgs <jmgs111@163.com>
2015-07-16 00:32:05 +0800

    home

--- src/com/test/JDBCTest.java
+++ src/com/test/JDBCTest.java
@@ -3,9 +3,7 @@ package com.test;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
-
 import org.junit.Test;
-
 import com.microsoft.sqlserver.jdbc.SQLServerDriver;
 
 public class JDBCTest {
@@ -17,7 +15,7 @@ public class JDBCTest {
 		String MysqlDriverStr = "com.mysql.jdbc.Driver";
 
 		String sqlserverUrl = "jdbc:sqlserver://localhost:1433;databaseName=test;integratedSecurity=true;";
-		String mysqlUrl = "jdbc:mysql://localhost/test1?useUnicode=true&characterEncoding=utf8";
+		String mysqlUrl = "jdbc:mysql://localhost/test1";
 		
 		String sqlserverUser = "sa";
 		String mysqlUser = "root";
@@ -25,12 +23,13 @@ public class JDBCTest {
 		String sqlserverPassword = "sharpower1*";
 		String mysqlPassword = "";
 		
-		Class.forName(MysqlDriverStr);
-		Connection connection = DriverManager.getConnection( mysqlUrl, mysqlUser, mysqlPassword );
+		
+		Class.forName(sqlserverDriverStr);
+		Connection connection = DriverManager.getConnection( sqlserverUrl, sqlserverUser, "" );
 		
 		Statement statment = connection.createStatement();
 		
-		statment.execute("INSERT INTO student(id, name, sex) VALUES(111,'小明','w' )");
+		statment.execute("INSERT INTO student(id, name, sex) VALUES(9,'小李','w' )");
 		
 		statment.close();
 		
